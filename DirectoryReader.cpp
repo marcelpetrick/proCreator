e50b107402b97e919dc4b7e7a6a00090063f2b68
Marcel Petrick <mail@marcelpetrick.it>
2017-10-22 12:44:23 +0200

    DirectoryReader: set up some hardcoed filtering for header-files

--- DirectoryReader.cpp
+++ DirectoryReader.cpp
@@ -4,6 +4,7 @@
 #include <QtCore/QDir>
 #include <QtCore/QDebug>
 
+// TODO add the path as parameter
 DirectoryReader::DirectoryReader()
 {
 
@@ -14,4 +15,14 @@ DirectoryReader::DirectoryReader()
 void DirectoryReader::getFiles()
 {
     qDebug() << "DirectoryReader::getFiles()"; //todom remove
+
+    QDir directory(".");
+
+    // initialize the filters
+    QStringList filter;
+    filter << "*.h" << "*.hpp"; // todo make more dynamic
+    //directory.setNameFilters(filter);
+
+    QStringList const fileNames = directory.entryList(filter, QDir::Files);
+    qDebug() << "fileNames:" << fileNames; //todom remove
 }
